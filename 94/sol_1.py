e44790385999e385c70b72e2a5ac94850e8990aa
lokeaichirou <32396024+lokeaichirou@users.noreply.github.com>
2022-06-13 19:16:15 +0800

    Create sol_1.py

---
+++ 94/sol_1.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
+        def dfs(root, res):
+            if not root:
+                return
+            
+            dfs(root.left, res)
+            res.append(root.val)
+            dfs(root.right, res)
+            
+            return res
+        
+        return dfs(root, [])
