9331ce8516a842ed159b74cca345f5c8e83c3f3c
lokeaichirou <32396024+lokeaichirou@users.noreply.github.com>
2022-06-14 12:53:30 +0800

    Create sol_1.py

---
+++ 108/sol_1.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        
+        if not nums:
+            return None
+        
+        mid_index = len(nums)//2
+        node = TreeNode(nums[mid_index])
+        
+        node.left =  self.sortedArrayToBST(nums[:mid_index])
+        node.right = self.sortedArrayToBST(nums[mid_index+1:])  # there is a need to take much care of here, it's easy to write "mid_index" instead of "mid_index+1"
+        
+        return node
