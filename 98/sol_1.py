acfb90cd8b43c9b86b0072200d5ff54e71e987a7
Li Mou <32396024+lokeaichirou@users.noreply.github.com>
2022-06-14 21:05:20 +0800

    Update sol_1.py

--- 98/sol_1.py
+++ 98/sol_1.py
@@ -7,6 +7,11 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         
+        # we perform depth-first Search and inorder traversal to obtain a list of all node values of binary tree, then judge if this list is 
+        # in ABSOLUTELY ascending order.
+        
+        # This method is quite cumbersome as it's both time and memory consuming because we need to collect all elements of binary tree...
+        
         if not root:
             return False
         
@@ -25,7 +30,7 @@ class Solution:
         InOrder(root, inorder_values)
         
         for i in range(len(inorder_values)-1):
-            if inorder_values[i] >= inorder_values[i+1]:
+            if inorder_values[i] >= inorder_values[i+1]:  # should be ">=" instead of ">" cuz of ABSOLUTELY ascending order
                 return False
         
         return True
