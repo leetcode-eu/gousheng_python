46ebbfc4afdab7496bd000131f7be566d34ea848
Li Mou <32396024+lokeaichirou@users.noreply.github.com>
2022-07-15 16:01:37 +0800

    Update sol_1.py

--- 39/sol_1.py
+++ 39/sol_1.py
@@ -1,6 +1,7 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        # recursion is used here to find all unique combinations of candidates whose sum equal to target
+        # recursion is used here to find all unique combinations of candidates whose sum equal to target ; 
+        # For correct path, we return the dfs when sum = target, otherwise for incorrect path(s), we break it when sum is definitely > target
         
         def dfs(cur_diff, path):
             if cur_diff == 0:  # current_sum = target
