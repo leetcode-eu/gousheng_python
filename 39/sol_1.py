81d130f3344a92afb4aed0f498cab158e9b37f95
Li Mou <32396024+lokeaichirou@users.noreply.github.com>
2022-07-15 15:30:32 +0800

    Update sol_1.py

--- 39/sol_1.py
+++ 39/sol_1.py
@@ -2,18 +2,22 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         # recursion is used here to find all unique combinations of candidates whose sum equal to target
         
-        def dfs(cur, path):
-            if cur == 0:
+        def dfs(cur_diff, path):
+            if cur_diff == 0:  # current_sum = target
                 res.append(path)
+                
             for n in candidates:
-                if n > cur:  # cuz candidates is sorted list
+                if n > cur_diff:  # the sum would be > target cuz candidates is sorted list
                     break
+                
                 if path and n < path[-1]:  # Due to requirement of unique combinations, can't use previous number used already
                     continue
                 
-                dfs(cur - n, path + [n])
+                # current_sum < target, so we add n to the path
+                dfs(cur_diff - n, path + [n])
                 
         res = []
         candidates.sort()
         dfs(target, [])
+        
         return res
