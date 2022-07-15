8a5fcb9ecbb706dadd44cc00ee3933296b18324c
Li Mou <32396024+lokeaichirou@users.noreply.github.com>
2022-07-15 16:09:48 +0800

    Update sol_1.py

--- 39/sol_1.py
+++ 39/sol_1.py
@@ -3,24 +3,21 @@ class Solution:
         # recursion is used here to find all unique combinations of candidates whose sum equal to target ; 
         # For correct path, we return the dfs when sum = target, otherwise for incorrect path(s), we break it when sum is definitely > target
         
-        def dfs(cur_diff, path):
+        def dfs(cur_diff, path, starting_index):
             if cur_diff == 0:  # current_sum = target
                 res.append(path)
                 return
                 
-            for num in candidates:
-                
-                if path and num < path[-1]:  # Due to requirement of unique combinations, can't use previous number used already
-                    continue
+            for num in candidates[starting_index:]:
                 
                 if num > cur_diff:  # the sum would be > target cuz candidates is sorted list
                     break
                 
-                # current_sum < target, so we add n to the path
-                dfs(cur_diff - num, path + [num])
-                
-        res = []
+                # current_sum < target, so we add num to the path
+                dfs(cur_diff - num, path + [num], candidates.index(num))
+        
         candidates.sort()
-        dfs(target, [])
+        res = []
+        dfs(target, [], 0)
         
         return res
