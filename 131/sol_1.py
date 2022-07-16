231951e78f4cd7d0457959940c376bc763090990
Li Mou <32396024+lokeaichirou@users.noreply.github.com>
2022-07-16 16:47:02 +0800

    Create sol_1.py

---
+++ 131/sol_1.py
@@ -0,0 +1,26 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        
+        if not s:
+            return []
+        
+        def IsPalindrome(partial_string):
+            return partial_string == partial_string[::-1]
+        
+        def dfs(input_string, combination):
+            
+            if len(input_string) == 0:
+                result.append(combination)
+                return
+            
+            for sep_idx in range(len(input_string)):
+                left = input_string[:sep_idx+1]
+                right = input_string[sep_idx+1:]
+
+                if IsPalindrome(left):
+                    dfs(right, combination+[left])
+            
+        result = []
+        dfs(s,[])
+        
+        return result
