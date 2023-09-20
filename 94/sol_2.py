61a1ab157c9fc1ec2170e4a2ebcc9c4bdd25bf5f
limou <li-mou.li-mou@inria.fr>
2023-09-20 23:41:24 +0200

    _

--- 94/sol_2.py
+++ 94/sol_2.py
@@ -11,33 +11,27 @@ class TreeNode:
 
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        
-        # Bread-first iteratoin
-        
+
+        if not root:
+            return []
+
         stack = [root]  # push root to the stack
         res = []  # save traversal result
-        
+
         while stack:
             node = stack.pop()
-            
-            if node:
-                stack.append(node.right)
-                stack.append(node)
-                stack.append(node.left)
-            else:  
-                # when the node is None node of leaf node
-                # Becase the number of None nodes = Number of nodes in sub-tree + 1,
-                # once we have None node, and when pop it out, it means we can pop sam enumber 
-                # of nodes out of stack
-                
-                if stack:  
-                
-                # Because the total number of None nodes in a binary tree = total number of nodes in
-                # binary tree, it means when we pop the last None node out of stack, there is no
-                # more available normal node to pop out IN THE STACK, so we need to add a if
-                # condition to prevent executing the following steps in the final loop
-                
-                    prev_node = stack.pop()
-                    res.append(prev_node.val)
-        
+
+            if isinstance(node, TreeNode):
+                if node.right is not None:
+                    stack.append(node.right)
+
+                stack.append(node.val)
+
+                if node.left is not None:
+                    stack.append(node.left)
+
+            else:
+                val = node
+                res.append(val)
+
         return res
