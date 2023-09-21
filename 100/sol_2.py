69cb2960defe86b31b241d7adbb9ad2f882577c1
limou <li-mou.li-mou@inria.fr>
2023-09-21 11:12:07 +0200

    --

--- 100/sol_2.py
+++ 100/sol_2.py
@@ -1,21 +1,33 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+
+from typing import Optional, List
+
+
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         
         # Another idea is to apply DFS for nodes of p tree and q tree SIMULTANEOUSLY
         # Previously, we perform DFS only on one tree's node, however, now we perform DFS
         # on p tree and q tree simultaneously to continuously check the equality of their nodes
-        
-        if p==None and q==None:  # reaching bottom Nones
+
+        # reaching bottom Nones
+        if p==None and q==None:
             return True
-        elif p==None or q==None: # only one of them is None
+
+        # only one of them is None
+        elif p==None or q==None:
             return False
-        elif p.val != q.val:     # both of them have values but different
+
+        # both of them have values but different
+        elif p.val != q.val:
             return False
-        else:                    # p.val = q.val
+
+        # p.val = q.val
+        else:
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
