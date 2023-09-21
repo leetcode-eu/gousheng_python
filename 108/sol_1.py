753cdd9b413e03de5c720138d166db48c86d70b5
limou <li-mou.li-mou@inria.fr>
2023-09-21 23:06:55 +0200

    --

--- 108/sol_1.py
+++ 108/sol_1.py
@@ -1,9 +1,14 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+
+from typing import Optional, List
+
+
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
         
