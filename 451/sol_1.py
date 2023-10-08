1a524bfd630d093d3e1032727a561abc8e412a66
limou <li-mou.li-mou@inria.fr>
2023-10-08 23:25:02 +0200

    -

---
+++ 451/sol_1.py
@@ -0,0 +1,11 @@
+
+import collections
+
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        hash_map = collections.Counter(s)
+        ans = ""
+        for c in sorted(hash_map, key = lambda item: hash_map[item], reverse = True):
+            ans += c * hash_map[c]
+
+        return ans
