d0684f411271b3f3232f16143bcf7dd979337ac1
limou <li-mou.li-mou@inria.fr>
2023-10-15 16:41:25 +0200

    --

---
+++ 1046/sol_1.py
@@ -0,0 +1,16 @@
+
+from typing import List
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones.sort()
+        while len(stones) > 1:
+            y = stones.pop()
+            x = stones.pop()
+
+            if x != y:
+                stones.append( y -x)
+
+            stones.sort()
+
+        return stones[0] if len(stones) == 1 else 0
